3b8f3821a456ef510cd6f92ca6c5763dc8a7dcfe
quan612 <quan612@yahoo.com>
2020-05-03 22:03:56 -0400

    push front end

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-import App from "../src/App/";
+import App from "./App";
 import "./shared/utils/fontawesome";
 
 ReactDOM.render(<App />, document.getElementById("root"));
