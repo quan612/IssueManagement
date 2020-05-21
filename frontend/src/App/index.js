24445d1715ba96d99a60d2f84a9ca0bdb048e527
quan612 <quan612@yahoo.com>
2020-05-20 23:36:15 -0400

    deploy production backend

--- frontend/src/App/index.js
+++ frontend/src/App/index.js
@@ -29,8 +29,8 @@ const cache = new InMemoryCache({
 const link = createHttpLink({
   credentials: "include",
   // uri: "http://localhost:5555/",
-  // uri: `https://jira-yoga-clone.herokuapp.com`,
-  uri: process.env.NODE_ENV === "development" ? devEndpoint : prodEndpoint,
+  uri: `https://jira-yoga-clone.herokuapp.com`,
+  // uri: process.env.NODE_ENV === "development" ? devEndpoint : prodEndpoint,
 });
 
 const client = new ApolloClient({
