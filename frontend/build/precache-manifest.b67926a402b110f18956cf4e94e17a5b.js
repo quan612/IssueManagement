5ec7bc5d1ac898f984af2fc32d932599cd27720d
quan612 <quan612@yahoo.com>
2020-07-18 00:05:56 -0400

    test build

---
+++ frontend/build/precache-manifest.b67926a402b110f18956cf4e94e17a5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b776e18942ff12064d6efbc55cb240e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67d8c375252eb3f674fc",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "4c9e8cef51fcceed6d4a",
+    "url": "/static/css/main.270e9b17.chunk.css"
+  },
+  {
+    "revision": "67d8c375252eb3f674fc",
+    "url": "/static/js/2.92e1abcf.chunk.js"
+  },
+  {
+    "revision": "9db28bd292481cf4df07d8b79bef7f38",
+    "url": "/static/js/2.92e1abcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9e8cef51fcceed6d4a",
+    "url": "/static/js/main.d4f1b0e0.chunk.js"
+  },
+  {
+    "revision": "3574459feb39962b2423",
+    "url": "/static/js/runtime-main.757f05e8.js"
+  }
+]);
