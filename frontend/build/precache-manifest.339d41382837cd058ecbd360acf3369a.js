da2fd40a6a6af6fea032b89bd1bd6ea60c4cbd1f
quan612 <quan612@yahoo.com>
2020-11-18 02:20:20 -0500

    test deploy

---
+++ frontend/build/precache-manifest.339d41382837cd058ecbd360acf3369a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1977254b881448e049c3ba25ba6db73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c65e6be22935630bd643",
+    "url": "/static/css/2.4ece9906.chunk.css"
+  },
+  {
+    "revision": "36034704d3bc88af9173",
+    "url": "/static/css/main.b131f4e4.chunk.css"
+  },
+  {
+    "revision": "c65e6be22935630bd643",
+    "url": "/static/js/2.10012404.chunk.js"
+  },
+  {
+    "revision": "cf3ba8f9ecacf96d9f55205314aba250",
+    "url": "/static/js/2.10012404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36034704d3bc88af9173",
+    "url": "/static/js/main.c29ffe07.chunk.js"
+  },
+  {
+    "revision": "8d3a5a211256d5f81c50",
+    "url": "/static/js/runtime-main.13c69f80.js"
+  }
+]);
--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -4,7 +4,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   content: [
     "./src/**/*.html",
     "./src/**/*.vue",
-    "./src/**/*.jsx",
+    //"./src/**/*.jsx",
     // etc.
   ],
 
@@ -21,7 +21,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 });
 
 module.exports = {
-  purge: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx", "public/**/*.html"],
+  purge: ["src/**/*.tsx", "public/**/*.html"],
   theme: {
     extend: {},
   },
